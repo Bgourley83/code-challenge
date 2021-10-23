539428df2664bf26c3ae4c8a8e3462677847f177
Bgourley83 <93051233+Bgourley83@users.noreply.github.com>
2021-10-23 15:02:52 -0400

    Create submission.js

---
+++ submission.js
@@ -0,0 +1,23 @@
+const findsum = function(array) {
+// (8,9,6) 
+} ; 23
+ 
+ const findFrequency(array) {
+ // (['k','j','k','h','j','h','k','k','f'])
+ }; // {most:'k', least:'f'}
+ 
+ const isPalindrome = function(str) {
+ // ( 'motor' ) 
+ }; // false
+ 
+ const largestPair = function(array) { 
+ // ([ 7,8,4,9,5 ])
+ } ; 15
+ 
+ const removeParenth = fuction(str) {
+ // ( 'ido(n't)likepizza' )
+ } ; // 'idolikepizza' 
+ 
+ const scoreScrabble = function(str) {
+ // * 1-a,b,c,d * 2-g,h,i,j * 3-l,m,n,o * 4-q,r,s,t ( 'brian' )
+ }; // 11
